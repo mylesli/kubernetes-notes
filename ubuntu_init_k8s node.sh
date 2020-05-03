0efd96a944915eb14d5e39faccf1ca5be8e1c7f2
mylesli <phsj1000@gmail.com>
2020-05-03 15:46:42 +0800

    Update ubuntu_init_k8s node.sh

--- ubuntu_init_k8s node.sh
+++ ubuntu_init_k8s node.sh
@@ -71,3 +71,43 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
+
+#2.k8s 前置作業關閉swap分區
+sudo  swapoff -a
+#要永久禁掉swap分区，打开如下文件注释掉swap那一行 （需要注释）
+sed -i '/\/dev/s/\/dev/#\/dev/g' /etc/fstab
+sed -i '/\/swap.img/s/\/swap.img/#\/swap.img/g' /etc/fstab
+#sed -i '/匹配字串/s/替換源字串/替換目標字串/g' filename ，\ 來跳脫字元 / 的干擾
+
+
+#3.同步時區
+sudo apt-get install ntp
+#sudo timedatectl list-timezones | grep Asia
+sudo timedatectl set-timezone Asia/Taipei
+sudo timedatectl set-ntp yes
+date
+#4.關閉SELinux
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+#4.改host(可不改) & hostname
+sudo sed -i 's/^preserve_hostname: false$/preserve_hostname: true/' /etc/cloud/cloud.cfg #這段一定要執行 不然rebook後hostname又恢復了
+#vi /etc/hosts
+#vi /etc/hostname
+
+#K8S安裝
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl #安裝完才設定cgroup
+
+
+
+#sudo sed -i '9 a/Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"'/etc/systemd/system/kubelet.service.d/10-kubeadm.conf #插入第九行
+sudo sed -i '$a\Environment="cgroup-driver=systemd/cgroup-driver=cgroupfs"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf #插入最後一行
+systemctl daemon-reload
+systemctl restart kubelet
+
